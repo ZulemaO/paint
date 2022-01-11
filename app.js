fa014ecf978296800a407c60b20dd2c37bb6bddd
ZulemaO <95653244+ZulemaO@users.noreply.github.com>
2022-01-11 12:49:45 -0600

    martes

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+//middlewares
+app.use(express.static('Public'))
+
+
+//Listen on port 3000
+server = app.listen(3000)
+
+
+//socket.io instantiation
+const io = require("socket.io")(server)
+
+
+//listen on every connection
+io.on('connection', (socket) => { 
+    socket.on('mouse', (data) => {
+        socket.broadcast.emit('painter', data);
+    });
+})
+
